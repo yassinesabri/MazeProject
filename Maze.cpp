e590d4b43d9c7f19842b3a0e10114c9ce30ac391
YASSINE SABRI <yassine.s.sabri@gmail.com>
2016-05-05 22:45:43 +0100

    Add Backtracking Algo
    Still Got some bugs and it crashs

--- Maze.cpp
+++ Maze.cpp
@@ -16,9 +16,13 @@ Maze::Maze(int h,int w)
     for(int i=0;i<h;i++)
         cell[i] = new char[w];
 
+    solution = new int*[h];
+    for(int i=0;i<h;i++)
+        solution[i] = new int[w];
+
     for(int i=0;i<h;i++)
         for(int j=0;j<w;j++)
-            cell[i][j]= 0;
+            {cell[i][j]= 0;solution[i][j]=0;}
 }
 void Maze::remove_wall(direction d, int i,int j)
 {
@@ -58,43 +62,89 @@ void Maze::show()
             cout<<endl;
         }
 }
-//void Maze::test()
-//{
-//    remove_wall(down,0,0);
-//    remove_wall(right,0,0);
-//    remove_wall(right,0,1);
-//    remove_wall(left,0,1);
-//    remove_wall(left,0,2);
-//    remove_wall(up,1,0);
-//    remove_wall(down,1,0);
-//    remove_wall(right,1,0);
-//    remove_wall(up,1,1);
-//    remove_wall(down,1,1);
-//    remove_wall(right,1,1);
-//    remove_wall(up,1,2);
-//    remove_wall(down,1,2);
-//    remove_wall(left,1,2);
-//    remove_wall(up,2,0);
-//    remove_wall(right,2,0);
-//    remove_wall(up,2,1);
-//    remove_wall(left,2,1);
-//    remove_wall(up,2,2);
-//    remove_wall(left,2,2);
-//        //Show cell's value
-//        for(int i=0; i<height; i++)
-//        {
-//            for(int j=0; j<width; j++)
-//            {
-//                //cout << cell[i][j];
-//                bitset<8> y(cell[i][j]);
-//                printf("%d ",y);
-//            }
-//            cout<<endl;
-//        }
-//
-//
-//
-//}
+bool Maze::is_safe(int i,int j,direction d)
+{
+    if(i==0 && j==0)
+        return true; //marque la cellule 0,0 dans le chemin sans condition
+    if(i>=0 && j>=0 && i<height && j<width)
+    {
+        if(d==down && cell[i][j]&1) //we check
+            return true;
+        if(d==right && cell[i][j]&8)
+            return true;
+        if(d==up && cell[i][j]&2)
+            return true;
+        if(d==left && cell[i-1][j]&4)
+            return true;
+    }
+    return false;
+}
+//Backtracking
+bool Maze::find_path(int i,int j,direction d)
+{
+    if(i==height-1 && j==width-1)
+    {
+        if(is_safe(i,j,d))
+        {
+            solution[i][j]=1;
+            return true;
+        }
+
+        return false;
+    }
+    if(is_safe(i,j,d))
+    {
+        //marquer la cellule dans le chemin
+        solution[i][j]=1;
+        if(d!=left && find_path(i,j+1,right)) //check right
+            return true;
+        if(d!=up && find_path(i+1,j,down)) //check down
+            return true;
+        if(d!=down && find_path(i-1,j,up)) //check up
+          return true;
+        if(d!=right && find_path(i,j-1,left)) //check left
+          return true;
+
+        solution[i][j]=0; //demarquer la cellule car le chemin est bloqué
+        return false;
+    }
+    return false;
+}
+void Maze::test()
+{
+        cout<<endl;
+        //Show cell's value
+        for(int i=0; i<height; i++)
+        {
+            for(int j=0; j<width; j++)
+            {
+                //cout << cell[i][j];
+                bitset<8> y(cell[i][j]);
+                printf("%d ",y);
+            }
+            cout<<endl;
+        }
+
+
+
+}
+void Maze::show_solution()
+{
+    find_path(0,0,right);
+    cout <<endl;
+    for(int i=0;i<height;i++)
+    {
+        for(int j=0;j<width;j++)
+        {
+            if(solution[i][j] == 1)
+                cout <<"# ";
+            else
+                cout<<"- ";
+        }
+        cout <<endl;
+    }
+
+}
 Maze::~Maze()
 {
     delete [] cell;
--- Maze.h
+++ Maze.h
@@ -10,11 +10,15 @@ class Maze
         char** cell;
         enum direction{up,down,right,left};
     public:
+        int** solution;
         Maze(int,int);
         virtual ~Maze();
         void remove_wall(direction,int i,int j);
         void show();
         void test();
+        bool find_path(int i,int j,direction d);
+        bool is_safe(int i,int j,direction d);
+        void show_solution();
     private:
 };
 
